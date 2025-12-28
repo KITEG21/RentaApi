643f5a0f28b704b6ab74ea19e7c1a772add251d0
KITEG21 <galau.delgado.rodriguez@gmail.com>
2025-12-28 13:29:15 -0500

    feat(migration): add payment props for tickets

---
+++ Renta.Infrastructure/Persistence/Migrations/20251228181817_TicketPaymentProps.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Renta.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TicketPaymentProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaymentDate",
+                table: "Tickets",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethod",
+                table: "Tickets",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentStatus",
+                table: "Tickets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentTransactionId",
+                table: "Tickets",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentDate",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentTransactionId",
+                table: "Tickets");
+        }
+    }
+}
